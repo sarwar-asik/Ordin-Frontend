8b1d5d71a677ebb8e8855aab00c7e1c8e6499d8c
sarwar-asik <sarwarasik@gmail.com>
2023-10-13 02:02:32 +0600

    header done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Header from "@/components/ui/Header";
 import Navbar from "@/components/ui/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
+      <Header></Header>
       <Navbar/>
       <h2>main routes</h2>
     </div>
---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,65 @@
+"use client";
+import React from "react";
+import { Collapse, Space } from "antd";
+import {
+  FacebookFilled,
+  TwitterCircleFilled,
+  PlusOutlined,
+  MinusOutlined,
+} from "@ant-design/icons";
+
+const Header = () => {
+  return (
+    <div className="py-2 bg-[#2B3445] text-white px-2">
+      <div className=" hidden  lg:flex justify-between items-center py-2">
+        <section className="flex items-center justify-start gap-2 text-white ">
+          <h2>
+            <FacebookFilled />
+          </h2>
+          <h2>
+            <TwitterCircleFilled />
+          </h2>
+        </section>
+        <section className="flex items-center justify-center gap-2">
+          <h3 className="px-3 bg-red-600 rounded-md">offer  </h3>
+          <p className="font-mono">10% Off per Square</p>
+        </section>
+      </div>
+      <Space className="w-full lg:hidden border-none" direction="vertical">
+        <Collapse
+          //   collapsible="header"
+          //   defaultActiveKey={["1"]}
+          items={[
+            {
+              key: "1",
+              label: (
+                <section className="flex items-center justify-end gap-2 text-white">
+                  <h3 className="px-3  bg-red-600 ">offer </h3>
+                  <p className="font-mono">10% Off per Square</p>
+                </section>
+              ),
+              children: (
+                <section className="flex items-center justify-start gap-2 text-white ">
+                  <h2>
+                    <FacebookFilled />
+                  </h2>
+                  <h2>
+                    <TwitterCircleFilled />
+                  </h2>
+                </section>
+              ),
+            },
+          ]}
+          ghost
+          expandIcon={() => (
+            <h1>
+              <PlusOutlined className="text-white font-extrabold" />
+            </h1>
+          )}
+        />
+      </Space>
+    </div>
+  );
+};
+
+export default Header;
