bbb95b387107c2a02dbde21dc2d619d448c2bcc8
sarwar-asik <sarwarasik@gmail.com>
2023-10-23 03:07:58 +0600

    rating done

--- src/components/Home/Rating.tsx
+++ src/components/Home/Rating.tsx
@@ -1,8 +1,20 @@
 "use client";
 import { useReviewsQuery } from "@/redux/api/ReviewApi";
-import { UserOutlined } from "@ant-design/icons";
-import { Layout, Card, Rate, List, Space, Avatar, Tooltip } from "antd";
+import { LeftOutlined } from "@ant-design/icons";
+import {
+  Layout,
+  Card,
+  Rate,
+  List,
+  Space,
+  Avatar,
+  Tooltip,
+  Image,
+  Row,
+  Col,
+} from "antd";
 import LoadingData from "../ui/Loader/LoadingData";
+import "./rating.css";
 
 const { Content } = Layout;
 
@@ -33,85 +45,94 @@ const ReviewsPage = () => {
 
   // console.log("🚀 ~ file: Rating.tsx:32 ~ ReviewsPage ~ data:", data);
   const reviewsData = data?.reviews;
-  console.log(
-    "🚀 ~ file: Rating.tsx:35 ~ ReviewsPage ~ reviewsDat:",
-    reviewsData
-  );
+  // console.log(
+  //   "🚀 ~ file: Rating.tsx:35 ~ ReviewsPage ~ reviewsDat:",
+  //   reviewsData
+  // );
 
   return (
-    <div className=" bg-secondary py- rounded-md px-1 ">
+    <div className=" bg-secondary py- rounded-md px-2 py-5">
       <section className="mb-5">
         <h1 className=" text-[2.3rem] font-serif my-3">Customer Reviews</h1>
         <p className="mt-3 font-medium font-sans">
           After satisfaction service our customer reviewed about our interior
-          about the interior service that he booked or choose.
+          about the interior service <br /> that he booked or choosed. Here showed some reviews.
         </p>
       </section>
+
+   
+
       <Layout>
         {isLoading && <LoadingData />}
-        <Content style={{ padding: "20px 0" }}>
-          <List
-            grid={{
-              gutter: 12,
-              xs: 1,
-              sm: 1,
-              md: 2,
-              lg: 5,
-            }}
-            dataSource={reviewsData}
-            renderItem={(item:any) => {
-              let reviews =""
-              if(item?.reviews?.length < 50){
-                 reviews=`${item?.reviews}.I am satisfied with the interior `
-              }
-              else{
-                reviews= item.reviews
-              }
-              return <List.Item>
-                <Card
-                  style={{
-                    borderRadius: "10px",
-                    padding: "0px",
-                    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-                    borderTopRightRadius: "30%",
-
-                    // margin: "8px",
-                  }}
-                >
-                  <div className="block lg:flex  items-center justify-between gap-2">
-                    <div style={{ display: "flex", alignItems: "center" }}>
-                      <Space wrap size={16}>
-                        <Tooltip title={item?.user?.name}>
-                          <Avatar
-                            shape="square"
-                            src={item?.user?.img}
-                            size={64}
-                            icon={item?.user?.name}
-                          />
-                        </Tooltip>
-                      </Space>
-                      {/* <span
-                          style={{
-                            marginLeft: "10px",
-                          }}
-                        >
-                          
-                        </span> */}
+        <Row gutter={[10, 10]}>
+          {reviewsData?.map((item: any, i) => {
+            const reviews =
+              item.reviews?.length > 64
+                ? item.reviews
+                : `${item?.reviews} Arranged my house with  the best interior design .`;
+            return (
+              <Col sm={24} lg={8} key={i}>
+                <section className=" w-[50% bg-white hover:shadow-xl ">
+                  <div
+                    className="relative   py bg-gra00"
+                    // style={{ border: "1px solid #ddd", borderRadius: "8px" }}
+                  >
+                    <div
+                      className="text-center  text-lg font-italic px-3 py-11"
+                      style={{
+                        border: "2px solid #ddd",
+                        borderRadius: "8px",
+                        // borderBottom:"px"
+                      }}
+                    >
+                      {reviews?.slice(0, 64)}
                     </div>
-                    <div className="">
-                      <Rate disabled defaultValue={item?.rating} />
-                    </div>
+                    <div
+                      className="absolute bg-red-30 bottom- left-1/2 transform -translate-x-1/2"
+                      style={{
+                        width: 0,
+                        height: 0,
+                        borderLeft: "28px solid transparent",
+                        borderRight: "28px solid transparent",
+                        borderTop: "36px solid #ddd",
+                        borderBottom: "0px ",
+                      }}
+                    ></div>
                   </div>
-                  <p style={{ color: "#94A3B8", marginTop: "10px" }}>
-                    {reviews?.slice(0, 64)} 
-                    {/* {item?.reviews?.length < 50 &&
-                      ` I am satisfied with the interior `} */}
-                  </p>
-                </Card>
-              </List.Item>;
-            }}
-          />
-        </Content>
+                  <section
+                    style={{
+                      // background:"red",
+                      display: "flex",
+                      textAlign: "center",
+                      justifyContent: "center",
+                      marginTop: "36px",
+                      // position:"absolute",
+                      // bottom: 0,
+                    }}
+                  >
+                    <div className=" p-1 ">
+                      <Tooltip title="sarwar">
+                        <Avatar
+                          shape="circle"
+                          size={64}
+                          src={item?.user?.img}
+                        />
+                      </Tooltip>
+                      <h3 className="text-slate-700 mt-1">Sarwar Hossain</h3>
+                      <Rate
+                        style={{
+                          fontSize: "16px",
+                        }}
+                        disabled
+                        defaultValue={4}
+                      />
+                    </div>
+                  </section>
+                </section>
+              </Col>
+            );
+          })}
+        </Row>
       </Layout>
     </div>
   );
