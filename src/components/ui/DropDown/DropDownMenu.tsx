19a52a1ca310f9987c3501d88536a0fb2517fa09
sarwar-asik <sarwarasik@gmail.com>
2023-11-06 15:51:16 +0600

    dropdown

--- src/components/ui/DropDown/DropDownMenu.tsx
+++ src/components/ui/DropDown/DropDownMenu.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { DownOutlined } from "@ant-design/icons";
 
 import { Dropdown, Space } from "antd";
-
+// ! start work
 const DropDownMenuUI = ({ name, items }: { name: string; items: any }) => (
   <Dropdown menu={{ items }} placement="bottom">
     <a onClick={(e) => e.preventDefault()}>
