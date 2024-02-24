0eed6a0e67175d2389c9d1a40c9ae9ce0dfef86e
sarwar-asik <sarwarasik@gmail.com>
2024-02-24 14:50:43 +0600

    not-foundPage=>updated

--- src/components/ui/DropDown/DropDownMenu.tsx
+++ src/components/ui/DropDown/DropDownMenu.tsx
@@ -5,12 +5,13 @@ import { DownOutlined } from "@ant-design/icons";
 import { Dropdown, Space } from "antd";
 // ! start work
 const DropDownMenuUI = ({ name, items }: { name: string; items: any }) => (
+  
   <Dropdown menu={{ items }} placement="bottom">
     <a onClick={(e) => e.preventDefault()}>
-      <Space>
-        {name}
+      <div className="flex  gap-1 items-center justify-center">
+        <span>{name}</span>
         <DownOutlined />
-      </Space>
+      </div>
     </a>
   </Dropdown>
 );
